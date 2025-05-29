c22b00659e50d45d7dff63e3137f6e4f68d36d08
aniket <aniketavinashmore343@gmail.com>
2025-05-30 01:13:00 +0530

    feat: only if error

--- pkg/middleware/adapter/erroradapter.go
+++ pkg/middleware/adapter/erroradapter.go
@@ -13,31 +13,33 @@ import (
 func ErrorAdapter(handler types.HandlerFuncWithErr) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		err := handler(c)
-		var apiError *internerrors.APIError
-		traceID := c.GetString(middleware.TraceIDKey)
-		path := c.GetString(middleware.PathKey)
-		method := c.GetString(middleware.MethodKey)
-		if errors.As(err, &apiError) {
-			c.JSON(apiError.GetStatus(), gin.H{
-				"status":  apiError.GetStatus(),
-				"code":    apiError.GetCode(),
-				"detail":  apiError.GetDetail(),
-				"traceId": traceID,
-			})
-			zap.L().Error(
-				apiError.Error(),
-				zap.String("traceId", traceID),
-				zap.String("path", path),
-				zap.String("method", method),
-			)
-		} else {
-			c.JSON(500, gin.H{"traceId": traceID, "status": 500, "code": "internal_server_error", "detail": "internal server error"})
-			zap.L().Error(
-				err.Error(),
-				zap.String("traceId", traceID),
-				zap.String("path", path),
-				zap.String("method", method),
-			)
+		if err != nil {
+			traceID := c.GetString(middleware.TraceIDKey)
+			path := c.GetString(middleware.PathKey)
+			method := c.GetString(middleware.MethodKey)
+			var apiError *internerrors.APIError
+			if errors.As(err, &apiError) {
+				c.JSON(apiError.GetStatus(), gin.H{
+					"status":  apiError.GetStatus(),
+					"code":    apiError.GetCode(),
+					"detail":  apiError.GetDetail(),
+					"traceId": traceID,
+				})
+				zap.L().Error(
+					apiError.Error(),
+					zap.String("traceId", traceID),
+					zap.String("path", path),
+					zap.String("method", method),
+				)
+			} else {
+				c.JSON(500, gin.H{"traceId": traceID, "status": 500, "code": "internal_server_error", "detail": "internal server error"})
+				zap.L().Error(
+					err.Error(),
+					zap.String("traceId", traceID),
+					zap.String("path", path),
+					zap.String("method", method),
+				)
+			}
 		}
 	}
 }
